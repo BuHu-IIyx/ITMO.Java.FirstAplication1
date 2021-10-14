67d239712eeae34e8bacd05002334c94623b2af1
Alexander (BuHu-IIyx) <Buhu.iiyxs@gmail.com>
2021-10-14 19:07:11 +0300

    Lab3

---
+++ src/itmo/java/labs/Lab3.java
@@ -0,0 +1,121 @@
+package itmo.java.labs;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Lab3 {
+    //
+    //Exercise 1
+    //
+    public static void ex1(){
+        int[] newArray = {1,2,3,4,3,6};
+        if(ex1CheckSortArray(newArray)){
+            System.out.println("OK");
+        }
+        else{
+            System.out.println("Please, try again");
+        }
+    }
+    public static boolean ex1CheckSortArray(int[] someArray){
+        boolean control = false;
+        for(int i = 1; i < Array.getLength(someArray); i++){
+            if(someArray[i-1] > someArray[i]){
+                control  = false;
+                break;
+            }
+            else{
+                control = true;
+            }
+        }
+        return control;
+    }
+    //
+    //Exercise 2
+    //
+    //Do this in second lab.
+    public static void ex2(){
+        int[] newArray = Lab2.ex5GetArray();
+        String newArrayString = Lab2.ex5ConvertToString(newArray);
+        System.out.println(newArrayString);
+    }
+    //
+    //Exercise 3
+    //
+    public static void ex3(){
+        int[] newArray = {1,2,3,4,3,6};
+        String newArrayString = Lab2.ex5ConvertToString(newArray);
+        System.out.println("Array 1: " + newArrayString);
+        int[] swap = swapArray(newArray);
+        String newArraySwapString = Lab2.ex5ConvertToString(swap);
+        System.out.println("Array 1: " + newArraySwapString);
+    }
+    public static int[] swapArray(int[] arrayForSwap){
+
+        int temp = arrayForSwap[0];
+        int arrayLength =Array.getLength(arrayForSwap);
+        arrayForSwap[0] = arrayForSwap[arrayLength-1];
+        arrayForSwap[arrayLength-1] = temp;
+        return arrayForSwap;
+    }
+
+    //
+    //Exercise 4
+    //
+    public static void ex4() {
+        int[] newArray = {4, 6, 3, 4, 3, 2, 6};
+        int arrayLength =Array.getLength(newArray);
+        boolean findFlag = false;
+
+        for (int i = 0; i<arrayLength; i++) {
+            int flag = 0;
+            for (int j = 0; j<arrayLength; j++) {
+                // Убираем одинаковые индексы
+                if(i == j){
+                    continue;
+                }
+                //Находим повторы
+                else if(newArray[i] == newArray[j]){
+                    flag += 1;
+                    break;
+                }
+            }
+            // Проверяем были ли повторы
+            if(flag == 0){
+                findFlag = true;
+                System.out.println("Первое уникальное число в массиве: " + newArray[i]);
+                break;
+            }
+        }
+        //Проверяем найдено ли хоть одно уникальное значение
+        if(!findFlag){
+            System.out.println("Уникальных чисел не найдено");
+        }
+    }
+    //
+    //Exercise 5
+    //
+    public static void ex5(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите порядковый номер числа Фибоначчи: ");
+        int nNumber = scanner.nextInt();
+        int count = 2;
+        int i = 0;
+        int j = 1;
+        while (count < nNumber){
+            count+=1;
+            int k = i + j;
+            i = j;
+            j = k;
+        }
+        System.out.println(j);
+    }
+    //
+    //Exercise 6
+    //
+    public static void ex6(){
+
+    }
+    //
+    //Exercise 7
+    //
+}
--- src/itmo/java/labs/Main.java
+++ src/itmo/java/labs/Main.java
@@ -4,18 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         //Lab 1
-//        Lab1.ex1();
-//        Lab1.ex2();
-//        Lab1.ex3();
-//        Lab1.ex4();
-//        Lab1.ex5();
-//        Lab1.ex6();
+    //        Lab1.ex1();
+    //        Lab1.ex2();
+    //        Lab1.ex3();
+    //        Lab1.ex4();
+    //        Lab1.ex5();
+    //        Lab1.ex6();
         //Lab 2
-//        Lab2.ex1();
-//        Lab2.ex2();
-//        Lab2.ex3();
-//        Lab2.ex4();
-//        Lab2.ex5();
-        Lab2.ex6();
+    //        Lab2.ex1();
+    //        Lab2.ex2();
+    //        Lab2.ex3();
+    //        Lab2.ex4();
+    //        Lab2.ex5();
+    //        Lab2.ex6();
+        //Lab 3
+//            Lab3.ex1();
+//            Lab3.ex2();
+//            Lab3.ex3();
+//            Lab3.ex4();
+            Lab3.ex5();
+
+
     }
 }
