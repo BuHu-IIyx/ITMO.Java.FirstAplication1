8a5827349d3b41e2d84bf0459646f7ad5885eaad
Alexander (BuHu-IIyx) <Buhu.iiyxs@gmail.com>
2021-10-24 00:52:27 +0300

    Lab4

--- src/itmo/java/labs/Main.java
+++ src/itmo/java/labs/Main.java
@@ -18,14 +18,19 @@ public class Main {
     //        Lab2.ex5();
     //        Lab2.ex6();
         //Lab 3
-//            Lab3.ex1();
-//            Lab3.ex2();
-//            Lab3.ex3();
-//            Lab3.ex4();
-//            Lab3.ex5();
-//            Lab3.ex6();
-            Lab3.ex7();
-
+    //            Lab3.ex1();
+    //            Lab3.ex2();
+    //            Lab3.ex3();
+    //            Lab3.ex4();
+    //            Lab3.ex5();
+    //            Lab3.ex6();
+    //            Lab3.ex7();
+        //Lab4
+//        Lab4.ex1();
+//        Lab4.ex2();
+//        Lab4.ex3();
+//        Lab4.ex4();
+        Lab4.ex5();
 
     }
 }
